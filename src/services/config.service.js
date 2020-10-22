823b12216c5ff300b07402242f1c2653bbf82625
Tom Fletcher <thoetc@bgs.ac.uk>
2020-10-22 09:11:37 +0100

    Connect to https

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'http://qa-quiz-api.us-west-2.elasticbeanstalk.com/';
+    service.apiBase = 'https://qa-quiz-api.us-west-2.elasticbeanstalk.com/';
 
     return service;
 }]);
